e40808d07c84123f4a1609710d1085764c2ea4f1
A-AlHirz <abdherz@hotmail.com>
2024-12-19 11:20:19 +0300

    Task3

--- ClassTask3/Program.cs
+++ ClassTask3/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task1:
+#region AllFunctions
+string FullName(string name)
+{
+    return name;
+}
+#endregion
+
+
+#region CallFunctions
+Console.WriteLine("Enter Name");
+string fname = Console.ReadLine();
+Console.WriteLine("Hello " + FullName(fname));
+
+#endregion
+
+
+//Task2:
+#region AllFunctions
+double Volume(double L , double W, double H )
+{
+    double vol = L * W * H;
+
+    return vol;
+}
+#endregion
+
+
+#region CallFunctions
+Console.WriteLine("----------------");
+
+Console.Write("Length: ");
+double L = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Width: ");
+double W = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Height: ");
+double H = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("The volume of the box= "+Volume( L,  W,  H));
+
+#endregion
+
+
+//Task3:
+#region AllFunctions
+double Number(double numb)
+{
+    double num
+}
+
+#endregion
+
+
+
+#region CallFunctions
+Console.WriteLine("----------------");
+
+Console.Write("Length: ");
+double L = Convert.ToInt32(Console.ReadLine());
+
+
+#endregion
